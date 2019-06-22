7712c77698d2a3305ce15224e05e37954ccdae48
SergioFaya <estisergio@gmail.com>
2019-06-22 15:33:17 +0200

    removed testing endpoint list

--- src/actions/githubAppTokenManager.js
+++ src/actions/githubAppTokenManager.js
@@ -10,20 +10,20 @@ module.exports = {
 			// read the pem certificate and then import it into rsa manager
 			var pemBuffer = config.resources.private_key_buffer;
 			if (config.resources.private_key_buffer == undefined) {
-				reject('Private key not found')
+				reject('Private key not found');
 			}
 			key.importKey(pemBuffer);
 			// export the key in the github api desired format
 			var private_key = key.exportKey('pkcs1-private');
 			generatePayload()
 				.then(payload => {
-					// we generate an access token for the app as installation 
+					// we generate an access token for the app as installation
 					var token = jwt.sign(payload, private_key, { algorithm: 'RS256' });
 					resolve(token);
 				})
 				.catch(err => {
 					reject(err);
-				})
+				});
 		});
 	},
 	checkAppToken: (token) => {
@@ -39,17 +39,17 @@ module.exports = {
 
 	}
 
-}
+};
 
 /**
  * Create the payload that will be sended in the auth token to github according with docs
  * https://developer.github.com/apps/building-github-apps/authenticating-with-github-apps/#authenticating-as-an-installation
- * note that ruby script in docs works but in nodejs we must trim 
+ * note that ruby script in docs works but in nodejs we must trim
  * the integer lenght to 10 digits as numbers in js are longer as they ARE FUCKING MILLISECONDS
  */
 function generatePayload() {
 	return new Promise((resolve, reject) => {
-		// Math.floor o Math.round 
+		// Math.floor o Math.round
 		var iat = Math.floor(Date.now() / 1000);
 		//iat = iat.substring(0, 10);
 		var exp = (Math.floor(Date.now() / 1000) + (10 * 60));
@@ -61,7 +61,7 @@ function generatePayload() {
 			exp,
 			// github app identifier
 			iss: config.oauth.app_id,
-		}
+		};
 		if (config.oauth.app_id == undefined) {
 			reject('App id not present');
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,21 +1,4 @@
 const app = module.exports = require('express')();
-
-app.get('/', (_req, res) => {
-	res.status(202).send(`<html>
-    <body>
-        <h1>Endpoints</h1>
-        <ul>
-            <li>/login</li>
-            <li>/login/check</li>
-            <li>/logout</li>
-            <li>/login</li>
-            <li>/installation</li>
-            <li>/installation/check</li>
-        </ul>
-    </body>
-</html>`);
-});
-
 app.use(require('./routerLogin'));
 app.use(require('./routerAuthInstallation'));
 
